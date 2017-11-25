e47430f17560a67626960830c272b95de5022c9c
charlie  lee <ctlee9096@gmail.com>
2017-11-25 16:05:52 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,8 @@
 class SessionsController < ApplicationController
   def create
     session[:name] = params[:name]
-     if session[:name].present? #def present?!blank? end From this we can say that an object will return true if it is NOT blank.
+     if session[:name].present? 
+       #def present?!blank? end From this we can say that an object will return true if it is NOT blank.
         #https://www.ruby-forum.com/topic/2306965
        redirect_to '/sessions/new'
      else
